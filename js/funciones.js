178ab858eecb10b714ef42e0a083f1a04628605d
Baggionet <baggio_2@hotmail.com>
2015-10-20 14:12:50 -0500

    haciendo cambios en 2 archivos
    se hacen cambios en html y js

--- js/funciones.js
+++ js/funciones.js
@@ -1,11 +1,10 @@
 
-//alert("funciona");
-
 function hideMenu(){
 	elMenu = document.getElementById('ulMenu');
 
-	elMenu.className = "visible-xs-block";
+	
 	
 }
 
 document.getElementById("menu").onclick=hideMenu;
+//alert( window.innerWidth + " x " + window.innerHeight);
