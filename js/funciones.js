5882a3d9c2672e6f5b4187f08ad5ee0404f3979f
Baggionet <baggio_2@hotmail.com>
2015-10-22 14:19:26 -0500

    agregamos funciones en la pagina
    Se agrego funcion para que la div velo este en el mismo tamaño que el
    body, tambien se agrego la funcion para cerrar la div velo.
    se agrego funcion para habilitar y deshabilitar el menu cuando estemos
    en dispositivos moviles telefonos y tables de 764 px de ancho.

--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,44 @@
 
+// estas 3 lineas al iniciar la pagina pondra automaticamente el tamaño del velo segun el tamaño del body
+var largo = document.body;
+var largotexto = largo.offsetHeight.toString();
+document.getElementById("elVelo").style.height = largotexto + "px";
+
+//largoVelo.style.height = largo.offsetHeight;
+//alert("esto es lo largo de la ventana " + largotexto + " px" );
+
+
+/*Funciones para distintas cosas.
+if (largo.offsetWidth > 768) {
+	alert("cambioi la resolucion");*/
+
+
+//funcion que habilita y deshabilita el meno cuando estamos en dispositivos pequeños
 function hideMenu(){
 	elMenu = document.getElementById('ulMenu');
+	//
+	if (elMenu.className == "oculto") 
+		{
+			elMenu.className = "visible-xs-block col-xs-4";
+		} 
+	else
+		{
+			elMenu.className = "oculto";
+		}
+	
+	
 
 	
 	
 }
 
+
+//Funcion que cierra la etiqueta DIV.velo 
+function cerrarVelo () {
+	var velo = document.getElementById("elVelo");
+	velo.className = "oculto";
+}
+
+document.getElementById("remover").onclick = cerrarVelo;
 document.getElementById("menu").onclick=hideMenu;
 //alert( window.innerWidth + " x " + window.innerHeight);
