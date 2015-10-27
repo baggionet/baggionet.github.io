5f28b0463a19ea3c6df58b433b21d8a6f69ed79d
Baggionet <baggio_2@hotmail.com>
2015-10-27 14:18:39 -0600

    Cambios en funciones.js y index
    En funciones.js agrege la funcion que muestra u oculta el boton de las
    imagenes y html cambie los nombres de los botones de "Ver mas1.." a solo
    "Ver mas.."

--- js/funciones.js
+++ js/funciones.js
@@ -12,33 +12,44 @@ document.getElementById("elVelo").style.height = largotexto + "px";
 if (largo.offsetWidth > 768) {
 	alert("cambioi la resolucion");*/
 
+//+++++++++++++++++++++++++++++++ funcion que habilita y deshabilita el menu cuando estamos en dispositivos pequeños +++++++++++++++++++++++//
+	function hideMenu(){
+		elMenu = document.getElementById('ulMenu');
+		//
+		if (elMenu.className == "oculto") 
+			{
+				elMenu.className = "visible-xs-block col-xs-4";
+			} 
+		else
+			{
+				elMenu.className = "oculto";
+			}	
+	}
 
-//funcion que habilita y deshabilita el meno cuando estamos en dispositivos pequeños
-function hideMenu(){
-	elMenu = document.getElementById('ulMenu');
-	//
-	if (elMenu.className == "oculto") 
-		{
-			elMenu.className = "visible-xs-block col-xs-4";
-		} 
-	else
-		{
-			elMenu.className = "oculto";
-		}
-	
-	
-
-	
-	
-}
-
-
-//Funcion que cierra la etiqueta DIV.velo 
+//++++++++++++++++++++++++++++++++ Funcion que cierra la etiqueta DIV.velo ++++++++++++++++++++++++++++++++++++++++++++//
 function cerrarVelo () {
 	var velo = document.getElementById("elVelo");
 	velo.className = "oculto";
 }
 
-document.getElementById("remover").onclick = cerrarVelo;
-document.getElementById("menu").onclick=hideMenu;
+//++++++ Funcion que habilita el boton "Ver mas.." que habilitara detalles de la imagen en la seccion "Mi trabajo"  +++++++++++++++++++++++++//
+
+function verDetalles (elEvento) {
+	var evento = elEvento || window.event;
+	var boton = document.getElementById("boton1-xs");
+	console.log(evento.type);
+	if (evento.type == "mouseover") 
+		{
+			boton.className = "boton btn btn-primary col-xs-4 col-xs-offset-4 visible-xs-block";
+		}
+	else
+		{
+			boton.className = "oculto";
+		}
+	}
+
+document.getElementById("img1").onmouseout = verDetalles;
+document.getElementById("img1").onmouseover = verDetalles;
+document.getElementById("remover").onclick = cerrarVelo;//Esta linea llama a la funcion "cerrarVelo" Al hacer click en el elemento con id "remover".
+document.getElementById("menu").onclick=hideMenu;//Esta linea llama a la funcion "hideMenu" Al hacer click en el elemento con id "menu".
 //alert( window.innerWidth + " x " + window.innerHeight);
