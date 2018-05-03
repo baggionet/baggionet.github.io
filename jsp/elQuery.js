e1ebe1d1e3a798818df00dcdfc9d319c350c619f
Baggionet <baggio_2@hotmail.com>
2018-05-02 23:23:51 -0500

    header terminado
    Se termino de configurar el header

--- jsp/elQuery.js
+++ jsp/elQuery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	// estas 3 lineas al iniciar la pagina pondra automaticamente el tamaño del velo segun el tamaño del body
-	var body = document.body;
+	/*var body = document.body;
 	var largoBody = body.offsetHeight.toString();
 	var anchoBody = body.offsetWidth.toString();
 	//document.getElementById("elVelo").style.height = largotexto + "px";
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	//elNav = document.getElementById("nav");
 	//largoNav=elNav.offsetHeight.toString();
 	//console.log("el largo del van es " +largoNav);
-	//elNav.offsetHeight.toString();
+	//elNav.offsetHeight.toString();*/
 
 
    
@@ -20,5 +20,19 @@ $(document).ready(function(){
 		$("#fonmenu").removeClass("oculto")
 		$("#fonmenu").addClass("col-xs-offset-6 col-xs-1")
 	}*/
-	
+var menufon = document.getElementById("menufon");
+var menuopen = document.getElementById("menuopen");
+var menuclose = document.getElementById("menuclose");
+
+	$("#menuopen").click(function(){
+		menufon.className = "col-xs-offset-3 col-xs-9 visible-xs-block";
+		menuopen.className = "oculto";
+		menuclose.className = "menumovil fa fa-bars";
+	});
+
+	$("#menuclose").click(function(){
+		menufon.className = "oculto";
+		menuclose.className = "oculto";
+		menuopen.className = "menumovil fa fa-bars";
+	});
 });
