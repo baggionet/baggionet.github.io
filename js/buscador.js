ca31a9ff53328733578f7c5b5079832dff62c2bd
Baggionet <baggio_2@hotmail.com>
2019-09-02 02:01:21 -0500

    Actualizacion de busqueda
    Se actualizan archivos de la app "busqueda"

--- js/buscador.js
+++ js/buscador.js
@@ -1,8 +1,16 @@
 //alert("funcionando");
-
+//Esta linea escucha cuando se le da click al boton buscar y ejecuta la funcion "validacion()"
 document.getElementById("buscar").addEventListener("click", validacion);
+document.getElementById("regreso").addEventListener("click", regresar);
+//funcion que sustituye "getElementById"
+function id (elId){
+    let id = document.getElementById(elId);
+    return id;
+}
 
-
+/*funcion que valida si el campo esta vacio o lleva una cadena de texto 
+ *y si es lo ultimo ejecuta la funcion "busqueda()" 
+*/
 function validacion(){
     let f = document.getElementById("busqueda").value;
     //alert(f);
@@ -11,13 +19,18 @@ function validacion(){
         alert("Ingresa la tienda a buscar");
     } else {
         //alert("buscando "  + f);
-        document.getElementById("busqueda").innerHTML = "";
         busqueda();
     }
 }
 
+/* funcion que realiza la busqueda de la tienda y muestra el resultado 
+ * en pantalla
+ *
+ * 
+*/ 
 function busqueda(){
-    
+    let nombre;
+    //let crTienda;
     let valor = document.getElementById("busqueda").value;
     valor = valor.toLowerCase();
    //alert(valor);
@@ -25,12 +38,30 @@ function busqueda(){
         console.log(conta);
         console.log(objeto.sucursales[conta].tienda);
         if(objeto.sucursales[conta].tienda == valor){
+            document.getElementById("busqueda").value = "";
+            let nav = id("nav");
+            nav.className = "oculto";
+            nombre = objeto.sucursales[conta].tienda;
+            //crTienda = objeto.sucursales[conta].crTienda;
             let mapa = objeto.sucursales[conta].ubicacion;
             //alert(mapa);
+            let conten = id("contenido");
+            conten.className = "row contenido";
+            document.getElementById("nombre").innerHTML = " " + nombre;
+            //document.getElementById("crTienda").innerHTML = crTienda;
             document.getElementById("mapa").innerHTML = mapa;
         }
-        
     }
+}
 
-    
+/*
+*Funcion que tendra la tarea de regresar a la
+*pagina principal
+*/
+function regresar(){
+    let conten = id("contenido");
+    conten.className = "oculto";
+
+    let nav = id("nav");
+    nav.className = "navbar navbar-expand-md navbar-dark";
 }
