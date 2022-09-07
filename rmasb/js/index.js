72db9351764a5b26d80c725c9712a146d6a6dacd
Baggionet <baggio_2@hotmail.com>
2022-09-06 23:54:05 -0500

    Update R+B
    Actualizacion de site R+B

--- rmasb/js/index.js
+++ rmasb/js/index.js
@@ -67,6 +67,61 @@ function pointMap(id){
     address.className = "address"
 }
 
+function menumovil() {
+    //var menuMovil = document.getElementById("menu-movil")
+    var icon = document.getElementsByTagName("i")
+    console.log(icon)  
+}
+//funciones para el menu responsivo
+var icon = document.getElementsByTagName("i")
+    //console.log(icon[0])
+//Activa el menu principal movil
+icon[0].addEventListener("click", function () {
+    var menuMovil = document.getElementById("menu-movil")
+    var principal = document.getElementById("principal")
+    icon[0].className = "bi bi-list d-none"
+    icon[1].className = "bi bi-x-lg"
+    menuMovil.className = "menu-movil activo-prinicipal"
+    principal.className = "menu-principal activo-prinicipal"
+})
+icon[1].addEventListener("click", function () {
+    var menuMovil = document.getElementById("menu-movil")
+    var principal = document.getElementById("principal")
+    var products = document.getElementById("products")
+    icon[0].className = "bi bi-list "
+    icon[1].className = "bi bi-x-lg d-none"
+    menuMovil.className = "menu-movil"
+    principal.className = "menu-principal d-none"
+    console.log(products.className)
+    if(products.className == "products-movil"){
+        products.className = "products-movil d-none"
+    }
+})
+function actProducts(){
+    var menuMovil = document.getElementById("menu-movil")
+    var principal = document.getElementById("principal")
+    var products = document.getElementById("products")
+    menuMovil.className = "menu-movil activo-products"
+    principal.className = "menu-principal d-none"
+    products.className = "products-movil"
+}
+function backPrincipal(){
+    var menuMovil = document.getElementById("menu-movil")
+    var principal = document.getElementById("principal")
+    var products = document.getElementById("products")
+    menuMovil.className = "menu-movil activo-prinicipal"
+    principal.className = "menu-principal activo-prinicipal"
+    products.className = "products-movil d-none"
+}
+//Blocke de codigo que activa el submenu de productos
+let ulProducts = document.getElementById("products")
+ulProducts.addEventListener("mouseover", function(){
+    let theProducts = document.getElementById("theProducts");
+    theProducts.className = "products"
+})
+theProducts.addEventListener("mouseout", function(){
+    theProducts.className = "products d-none"
+}) 
 
 
 
