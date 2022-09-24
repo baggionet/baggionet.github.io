f61e3550f891f226d536a9f47cc7baa8d1a0dbb2
Baggionet <baggio_2@hotmail.com>
2022-09-24 15:58:02 -0500

    Actualizacion de la animacion
    Se realizo actualizacion  y se finalizo las animaciones del texto e imagen del background

--- rmasb/js/app.js
+++ rmasb/js/app.js
@@ -1,5 +1,5 @@
-//window.addEventListener('load', function() {   
-//})
+// 
+//Funcion que controla la animacion de las letras del header de la pagina Nosotros
 function animeSliderNosotros() {
     $('.vertical-nosotros').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
         $('#slider-nosotros').animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
@@ -16,6 +16,7 @@ function animeSliderNosotros() {
         } );
     });
 }
+//Funcion que controla la animacion de las letras del header de la pagina Hiphos
 function animeSliderHiphos() {
     $('.vertical-hiphos').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
         $("#slider-hiphos").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
@@ -33,6 +34,112 @@ function animeSliderHiphos() {
         } );
     });
 }
+//Funcion que controla la animacion de las letras del header de la pagina Biohydra
+function animeSliderBiohydra() {
+    $('.vertical-biohydra').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-biohydra").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-biohydra').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-biohydra').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-biohydra').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-biohydra').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        $('.horizontal-text5-biohydra').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                            $('.recuadro-biohydra').slideDown("slow" ).css({"display": "flex"});
+                        } );
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+//Funcion que controla la animacion de las letras del header de la pagina Bioenergy
+function animeSliderBioenergy() {
+    $('.vertical-bioenergy').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-bioenergy").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-bioenergy').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-bioenergy').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-bioenergy').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-bioenergy').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        $('.horizontal-text5-bioenergy').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                            $('.recuadro-bioenergy').slideDown("slow" ).css({"display": "flex"});
+                        } );
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+//Funcion que controla la animacion de las letras del header de la pagina Ganatec
+function animeSliderGanatec() {
+    $('.vertical-ganatec').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-ganatec").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-ganatec').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-ganatec').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-ganatec').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-ganatec').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        $('.horizontal-text5-ganatec').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                            $('.recuadro-ganatec').slideDown("slow" ).css({"display": "flex"});
+                        } );
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+//Funcion que controla la animacion de las letras del header de la pagina Grasssuplex
+function animeSliderGrasssuplex() {
+    $('.vertical-grasssuplex').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-grasssuplex").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-grasssuplex').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-grasssuplex').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-grasssuplex').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-grasssuplex').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        $('.horizontal-text5-grasssuplex').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                            $('.recuadro-grasssuplex').slideDown("slow" ).css({"display": "flex"});
+                        } );
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+//Funcion que controla la animacion de las letras del header de la pagina Pganatec
+function animeSliderPganatec() {
+    $('.vertical-pganatec').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-pganatec").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-pganatec').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-pganatec').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-pganatec').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-pganatec').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        console.log($(".recuadro-pganatec"))
+                        $('.recuadro-pganatec').slideDown("slow" ).css({"display": "flex"});
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+//Funcion que controla la animacion de las letras del header de la pagina Plechero
+function animeSliderPlechero() {
+    $('.vertical-plechero').animate( {"marginLeft":0,"opacity":"show"}, 200, "linear", function() {
+        $("#slider-plechero").animate( {"marginRight":0,"opacity":"show"}, 2000, "linear");
+        $('.horizontal-text1-plechero').animate( {"marginLeft":"18px","opacity":"show"}, 500, "linear", function(){
+            $('.horizontal-text2-plechero').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                $('.horizontal-text3-plechero').animate( {"marginLeft":"19px","opacity":"show"}, 500, "linear", function(){
+                    $('.horizontal-text4-plechero').animate( {"marginLeft":"20px","opacity":"show"}, 500, "linear", function(){
+                        console.log($(".recuadro-plechero"))
+                        $('.recuadro-plechero').slideDown("slow" ).css({"display": "flex"});
+                    } );
+                } );
+            } );
+        } );
+    });
+}
+animeSliderPlechero()
+animeSliderPganatec()
+animeSliderGrasssuplex()
+animeSliderGanatec()
+animeSliderBioenergy()
+animeSliderBiohydra()
 animeSliderNosotros()
 animeSliderHiphos()
 //slide down menu productos
