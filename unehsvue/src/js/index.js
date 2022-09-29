a7165a26686064d97340982299578fd5ac4119c6
Baggionet <baggio_2@hotmail.com>
2022-09-29 18:32:03 -0500

    FInalizacion rmasb
    Se llego a la finalizacion con el cliente

--- unehsvue/src/js/index.js
+++ unehsvue/src/js/index.js
@@ -1,5 +1,45 @@
+const sitosInteres =  {
+    template:
+    `
+    <div class="sitios-interes">
+        <ul class="sitios">
+            <li><img src="./src/assets/Sitiosdeinteres.svg"/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+            <li><img src=""/></li>
+        </ul>
+    </div>
+    `
+}
 //alert("cargo......")
-var app = new Vue({
-    el: "#app",
-    vuetify: new Vuetify(),
+//se define el componente de la ruta
+const blog = {
+    template:`<h2>Aqui es el blog </h2>`
+}
+const contacto = {
+    template:`<h2>Aqui contactanos </h2>`
+}
+//Se definen las rutas
+const routes = [
+    {path: "/blog", component: blog, },
+    {path: "/contacto", component: contacto},
+    {path: "/", redirect: "/blog"}
+]
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
 })
+const app = Vue.createApp({})
+app.use(router)
+app.mount('#app')
+
+
+/*var app = new Vue({
+    el: "#app",
+    //vuetify: new Vuetify(),
+    router,
+})*/
